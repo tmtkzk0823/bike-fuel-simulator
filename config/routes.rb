394381d2b8fef51f2a3e52e6be3e6ece4b90e037
tomita_kazuki <tmtkzk@icloud.com>
2022-11-11 10:50:02 +0900

    Add: bikesコントローラーindexアクチョンのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :manufacturers, only: %i[index]
+      resources :bikes, only: %i[index]
     end
   end
 end
