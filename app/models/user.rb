9d2c7da8c74b597319b1eb016c501d2f836c414b
tomita_kazuki <tmtkzk@icloud.com>
2022-12-10 13:25:41 +0900

    Add: devise導入によるUserテーブルを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-  has_many :user_bikes, dependent: :destroy
-  has_many :visited_places, dependent: :destroy
+# Include default devise modules.
+devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable,
+        :confirmable, :omniauthable
+include DeviseTokenAuth::Concerns::User
+has_many :visited_places, dependent: :destroy
+has_many :user_bikes, dependent: :destroy
 end
