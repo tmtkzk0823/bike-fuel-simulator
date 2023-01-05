2c2b91cd8247714a4d93fd79e01ec200deccd9c5
tomita_kazuki <tmtkzk@icloud.com>
2023-01-05 22:29:52 +0900

    Add: 編集機能で使う記述を追加

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,10 +1,18 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
 
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_current_password(params) #独自のメソッド。 編集する際にpasswordを入力する必要をなくす
+  end
+
   private
 
   def sign_up_params
     params.require(:registration).permit(:name, :email, :password, :password_confirmation)
   end
 
-
+  def account_update_params
+    params.require(:registration).permit(:name, :registration, :email, :image)
+end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :verify_authenticity_token, if: :devise_controller?
   helper_method :current_user, :user_signed_in?
 
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  private
+
+  # アカウント編集に使用
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
