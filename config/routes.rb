0da642fbfd08b37121c974353f8bb4c2a9d698b0
tomita_kazuki <tmtkzk@icloud.com>
2023-01-13 11:13:15 +0900

    Add: user_bikesコントローラーへのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,12 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
-      
+
       resources :manufacturers do
         resources :bikes, only: %i[index]
       end
+
+      resources :user_bikes, only: %i[create destroy]
     end
   end
 end
