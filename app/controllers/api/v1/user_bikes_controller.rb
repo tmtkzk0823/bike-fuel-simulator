4b37a236d5a3f7862ccae4bab5a2d7ecb5860078
tomita_kazuki <tmtkzk@icloud.com>
2023-01-13 16:30:22 +0900

    Add: createメソッド、destroyメソッドを作成

--- app/controllers/api/v1/user_bikes_controller.rb
+++ app/controllers/api/v1/user_bikes_controller.rb
@@ -1,10 +1,23 @@
 class Api::V1::UserBikesController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def create
-    
-  end
+    if current_user 
+      current_user.user_bikes.create!(user_bike_paramas)
+      user_bikes = UserBike.where(user_id: current_user.id)
+      render json: user_bikes
+    end
 
   def destroy
+    user_bike = UserBike.find(params[:id])
+    user_bike.destroy
+    render json: user_bike
 
+  end
+
+  private
+
+  def user_bike_paramas
+    params.require(:user_bike).permit(:bike_id)
   end
 end
